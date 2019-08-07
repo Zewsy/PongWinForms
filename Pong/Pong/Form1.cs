675dfb950b4326b8430c254d2eb23f0849fdf4e9
Zoltán Lőrincz <zoltan9811@gmail.com>
2019-08-07 19:02:54 +0200

    Classes + init added, test Graphics lines

--- Pong/Pong/Form1.cs
+++ Pong/Pong/Form1.cs
@@ -12,9 +12,21 @@ namespace Pong
 {
     public partial class Form1 : Form
     {
+        Racket racket;
+        Ball ball;
         public Form1()
         {
+            racket = new Racket();
+            ball = new Ball();
             InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.FillRectangle(Brushes.Gray, new Rectangle(20, 150, 5, 50));
+            e.Graphics.FillRectangle(Brushes.Gray, new Rectangle(550, 150, 5, 50));
+            e.Graphics.FillEllipse(Brushes.White, new RectangleF(280, 150, 15, 15));
         }
     }
 }
---
+++ Pong/Pong/Racket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong
+{
+    class Racket
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
